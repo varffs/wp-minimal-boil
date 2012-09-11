e163ae9baef211b47256a8da3a6b9d39b6ca3e3e
Patrick Best <patrickbest@patrickbest.com>
2012-09-11 17:54:49 +0100

    better js handling
    updated cdn jquery url, added fallback/local jq, set up lib,js bundle
    for production, added prefix free to lib, named script.js the much
    better name my.js :}

--- functions.php
+++ functions.php
@@ -1,21 +1,30 @@
 <?php 
 function my_scripts_method() {
     wp_deregister_script( 'jquery' );
-    wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js');
+/* use google cdn jquery for potential cache speed advantage */
+    wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js');
     wp_enqueue_script('jquery','','','',true);
-//  wp_enqueue_script( 'jquery-ui-draggable','','','',true );
+/* wp includes quite a few js libs by default, here is the ui draggable */
+/* wp_enqueue_script( 'jquery-ui-draggable','','','',true ); */
     $templateuri = get_template_directory_uri() . '/js/';
-// follow this pattern to enqueue your scripts
-    $myscripts = $templateuri."script.js";
+/* fallback or local jquery - remove on production if u want */
+    $localjq = $templateuri."jquery-1.8.1.min.js";
+    wp_enqueue_script( 'localjq', $localjq,'','',true);
+/* lib to bundle production js libaries */
+    $jslib = $templateuri."lib.js";
+    wp_enqueue_script( 'jslib', $jslib,'','',true);
+/* follow this pattern to enqueue your scripts */
+    $myscripts = $templateuri."my.js";
     wp_enqueue_script( 'myscripts', $myscripts,'','',true);
 }     
-	add_action('wp_enqueue_scripts', 'my_scripts_method');
-
+add_action('wp_enqueue_scripts', 'my_scripts_method');
 	
 if ( function_exists( 'add_theme_support' ) ) { 
   	add_theme_support( 'post-thumbnails' ); 
 }
-
+if ( function_exists( 'add_image_size' ) ) { 
+	add_image_size( 'name', 199, 299, true );
+}
 
 get_template_part( 'lib/meta-boxes' );
 add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 9999 );
---
+++ js/my.js
@@ -0,0 +1,32 @@
+/* Author:  */
+/* JQuery doc ready - you can use the $ shortcut inside here: */
+jQuery(document).ready(function() {
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
