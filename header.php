3d7733e70971e78c8b7bc9b51e121b37579fb751
Patrick Best <patrickbest@patrickbest.com>
2014-02-11 19:37:15 -0600

    header post meta fix & stylus fix
    put some of the php for the meta in a conditional to avoid warnings on
    404 pages. added placeholder so stylus compiles out the box

--- header.php
+++ header.php
@@ -11,9 +11,14 @@
 <!-- social graph meta -->
 
 	<meta name="twitter:site" value="@">
-	<?php if (have_posts()):while(have_posts()):the_post(); endwhile; endif;
+	<?php if (have_posts()):while(have_posts()):the_post();
 		$excerpt = get_the_excerpt();
-		if(has_post_thumbnail()) { $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'opengraph' ); ?>
+		if(has_post_thumbnail()) {
+			$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'opengraph' );
+		}
+		endwhile; endif;
+		if (!empty($thumb)) {
+	?>
 	<meta property="og:image" content="<?php echo $thumb['0'] ?>" />
 		<?php } else { ?>
 	<meta property="og:image" content="<?php bloginfo('stylesheet_directory'); ?>/img/favicon.png" />
